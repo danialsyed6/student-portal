a8553f2365bb5b10b59cabe54d7e704909c86d29
Danial Syed <danial@mergestack.com>
2022-09-13 12:08:24 +0500

    Structure Changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
-import { AddStudent, Dashboard, EditStudent } from './views/pages';
+import { Dashboard } from './containers';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/addStudent" element={<AddStudent />} />
-        <Route path="/editStudent" element={<EditStudent />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './redux';
+import store from './state';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
